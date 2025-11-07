5601d96797d5077b88b120c6b7c04c2981a3169c
Varun Bhandary <varun.bhandary@databricks.com>
2025-11-07 22:06:24 +0000

    ui: Update view icon to match Databricks design
    Replaced generic graph icon with Databricks' official view icon:
    - Sidebar.tsx: Updated view icon in tree display
    - ViewDetails.tsx: Updated icon in header and dependencies list
    - Used inline SVG (from Databricks UI) instead of codicon
    
    Benefits:
    - Better visual consistency with Databricks UI
    - Clearer distinction between views and other objects
    - More professional appearance
    
    Technical details:
    - SVG uses 1em sizing for proper scaling
    - fill="currentColor" for theme compatibility
    - Added marginRight for proper spacing in dependency list

--- packages/vscode-extension/src/webview/components/Sidebar.tsx
+++ packages/vscode-extension/src/webview/components/Sidebar.tsx
@@ -662,7 +662,10 @@ export const Sidebar: React.FC = () => {
                           }}
                         >
                           <span className="icon">
-                            <i className="codicon codicon-graph-line"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
+                              <path fill="currentColor" fillRule="evenodd" d="M1.75 1a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75H4v-1.5H2.5V7H5v2h1.5V7h3v2H11V7h2.5v2H15V1.75a.75.75 0 0 0-.75-.75zM13.5 5.5v-3h-11v3z" clipRule="evenodd"></path>
+                              <path fill="currentColor" fillRule="evenodd" d="M11.75 10a.75.75 0 0 0-.707.5H9.957a.75.75 0 0 0-.708-.5H5.75a.75.75 0 0 0-.75.75v1.75a2.5 2.5 0 0 0 5 0V12h1v.5a2.5 2.5 0 0 0 5 0v-1.75a.75.75 0 0 0-.75-.75zm.75 2.5v-1h2v1a1 1 0 1 1-2 0m-6-1v1a1 1 0 1 0 2 0v-1z" clipRule="evenodd"></path>
+                            </svg>
                           </span>
                           <span className="name">{view.name}</span>
                           <span className="badge" style={{background: 'var(--vscode-charts-blue)'}}>VIEW</span>
--- packages/vscode-extension/src/webview/components/ViewDetails.tsx
+++ packages/vscode-extension/src/webview/components/ViewDetails.tsx
@@ -62,7 +62,10 @@ export const ViewDetails: React.FC<ViewDetailsProps> = ({ viewId }) => {
       {/* Header */}
       <div className="view-header">
         <div className="view-title">
-          <i className="codicon codicon-graph-line"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
+            <path fill="currentColor" fillRule="evenodd" d="M1.75 1a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75H4v-1.5H2.5V7H5v2h1.5V7h3v2H11V7h2.5v2H15V1.75a.75.75 0 0 0-.75-.75zM13.5 5.5v-3h-11v3z" clipRule="evenodd"></path>
+            <path fill="currentColor" fillRule="evenodd" d="M11.75 10a.75.75 0 0 0-.707.5H9.957a.75.75 0 0 0-.708-.5H5.75a.75.75 0 0 0-.75.75v1.75a2.5 2.5 0 0 0 5 0V12h1v.5a2.5 2.5 0 0 0 5 0v-1.75a.75.75 0 0 0-.75-.75zm.75 2.5v-1h2v1a1 1 0 1 1-2 0m-6-1v1a1 1 0 1 0 2 0v-1z" clipRule="evenodd"></path>
+          </svg>
           <h2>{catalog.name}.{schema.name}.{view.name}</h2>
         </div>
         <span className="view-badge">VIEW</span>
@@ -156,7 +159,10 @@ export const ViewDetails: React.FC<ViewDetailsProps> = ({ viewId }) => {
                 <ul className="dependency-list">
                   {view.extractedDependencies.views.map((v: string, i: number) => (
                     <li key={i}>
-                      <i className="codicon codicon-graph-line"></i>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 16 16" aria-hidden="true" focusable="false" style={{ marginRight: '4px' }}>
+                        <path fill="currentColor" fillRule="evenodd" d="M1.75 1a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75H4v-1.5H2.5V7H5v2h1.5V7h3v2H11V7h2.5v2H15V1.75a.75.75 0 0 0-.75-.75zM13.5 5.5v-3h-11v3z" clipRule="evenodd"></path>
+                        <path fill="currentColor" fillRule="evenodd" d="M11.75 10a.75.75 0 0 0-.707.5H9.957a.75.75 0 0 0-.708-.5H5.75a.75.75 0 0 0-.75.75v1.75a2.5 2.5 0 0 0 5 0V12h1v.5a2.5 2.5 0 0 0 5 0v-1.75a.75.75 0 0 0-.75-.75zm.75 2.5v-1h2v1a1 1 0 1 1-2 0m-6-1v1a1 1 0 1 0 2 0v-1z" clipRule="evenodd"></path>
+                      </svg>
                       <code>{v}</code>
                     </li>
                   ))}
